967f9b1c74fb8504ec0e2d68860e4c983f449909
brandly <brandly13@gmail.com>
2016-07-14 13:51:48 -0400

    init

---
+++ cli.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const moment = require('moment')
+const _ = require('lodash')
+
+const baseUrl = 'https://clients6.google.com/calendar/v3/calendars/0pa55cqrvtpcalggvn5mr0kpio@group.calendar.google.com/events'
+
+axios.get(baseUrl, {
+  params: {
+    calendarId: '0pa55cqrvtpcalggvn5mr0kpio@group.calendar.google.com',
+    singleEvents: true,
+    timeZone: 'America/New_York',
+    maxAttendees: 1,
+    maxResults: 250,
+    timeMin: moment().toISOString(),
+    timeMax: moment().add(4, 'days').toISOString(),
+    orderBy: 'startTime',
+    key: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs'
+  }
+}).then((res) => {
+  const items = res.data.items
+  const groupedItems = _.groupBy(items, (item) => item.start.date)
+
+  Object.keys(groupedItems).forEach((date) => {
+    const trucksOnDate = groupedItems[date]
+
+    const usefulDate = moment(date)
+    console.log(usefulDate.format('dddd, MMMM Do'))
+
+    trucksOnDate.forEach((truck) => {
+      console.log(`\t${truck.summary}`)
+    })
+
+    console.log('')
+  })
+}).catch((err) => {
+  console.error(err)
+})
